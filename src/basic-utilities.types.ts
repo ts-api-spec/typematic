5764ec1251d2d7347f26dc2b26e008fde37ebcdb
ecyrbe <ecyrbe@gmail.com>
2023-06-12 17:27:50 +0200

    feat(utility): add some basic utility

--- src/basic-utilities.types.ts
+++ src/basic-utilities.types.ts
@@ -1,13 +1,4 @@
-import type {
-  ApiSpec,
-  ApiMethod,
-  ApiDataParameter,
-  ApiBaseMetadata,
-  ApiEndpoint,
-  ApiParameter,
-} from "./api-spec.types";
-import type { InferInputTypeFromSchema, SchemaType } from "./schema-type.types";
-import { ApiTypeScriptSchema } from "./schema-type-ts";
+import type { ApiSpec, ApiMethod, ApiDataParameter } from "./api-spec.types";
 
 export type ApiGetEndpoint<
   Api extends ApiSpec,
@@ -48,6 +39,12 @@ export type ApiGetEndpointParams<
   Endpoint extends keyof Api["endpoints"]
 > = ApiGetEndpoint<Api, Endpoint>["params"];
 
+export type ApiGetEndpointParam<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Param extends keyof ApiGetEndpoint<Api, Endpoint>["params"]
+> = ApiGetEndpoint<Api, Endpoint>["params"][Param];
+
 export type ApiGetEndpointQueries<
   Api extends ApiSpec,
   Endpoint extends keyof Api["endpoints"]
@@ -64,10 +61,27 @@ export type ApiGetEndpointHeaders<
   Endpoint extends keyof Api["endpoints"]
 > = ApiGetEndpoint<Api, Endpoint>["headers"];
 
+export type ApiGetEndpointHeader<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Header extends keyof ApiGetEndpoint<Api, Endpoint>["headers"]
+> = ApiGetEndpoint<Api, Endpoint>["headers"][Header];
+
 export type ApiGetEndpointCookies<
   Api extends ApiSpec,
   Endpoint extends keyof Api["endpoints"]
 > = ApiGetEndpoint<Api, Endpoint>["cookies"];
+
+export type ApiGetEndpointCookie<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Cookie extends keyof ApiGetEndpoint<Api, Endpoint>["cookies"]
+> = ApiGetEndpoint<Api, Endpoint>["cookies"][Cookie];
+
+export type ApiGetEndpointResponses<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"]
+> = ApiGetEndpoint<Api, Endpoint>["responses"];
 
 export type ApiGetEndpointResponseByStatus<
   Api extends ApiSpec,
@@ -81,109 +95,3 @@ export type ApiGetEndpointBodySchema<
 > = ApiGetEndpointBody<Api, Endpoint> extends ApiDataParameter
   ? ApiGetEndpointBody<Api, Endpoint>["schema"]
   : never;
-
-export type ApiGetSchemaType<
-  Api extends ApiSpec,
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema
-> = Api["metadata"] extends infer Meta extends ApiBaseMetadata
-  ? Meta["schemaType"] extends infer Type extends SchemaType
-    ? Type
-    : DefaultSchemaType
-  : DefaultSchemaType;
-
-export type ApiGetEndpointSchemaType<
-  Api extends ApiSpec,
-  Endpoint extends keyof Api["endpoints"],
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema,
-  ComputedEndpoint extends ApiEndpoint = ApiGetEndpoint<Api, Endpoint>
-> = ComputedEndpoint["metadata"] extends infer Meta extends ApiBaseMetadata
-  ? Meta["schemaType"] extends infer Type extends SchemaType
-    ? Type
-    : ApiGetSchemaType<Api, DefaultSchemaType>
-  : ApiGetSchemaType<Api, DefaultSchemaType>;
-
-export type ApiGetEndpointBodySchemaType<
-  Api extends ApiSpec,
-  Endpoint extends keyof Api["endpoints"],
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema
-> = ApiGetEndpointBody<
-  Api,
-  Endpoint
-> extends infer Param extends ApiDataParameter
-  ? Param["metadata"] extends infer Meta extends ApiBaseMetadata
-    ? Meta["schemaType"] extends infer Type extends SchemaType
-      ? Type
-      : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>
-    : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>
-  : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>;
-
-export type ApiGetEndpointParamSchemaType<
-  Api extends ApiSpec,
-  Endpoint extends keyof Api["endpoints"],
-  PathParam extends keyof ApiGetEndpointParams<Api, Endpoint>,
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema
-> = ApiGetEndpointParams<
-  Api,
-  Endpoint
->[PathParam] extends infer Param extends ApiDataParameter
-  ? Param["metadata"] extends infer Meta extends ApiBaseMetadata
-    ? Meta["schemaType"] extends infer Type extends SchemaType
-      ? Type
-      : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>
-    : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>
-  : ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>;
-
-export type ApiInferEndpointBody<
-  Api extends ApiSpec,
-  Endpoint extends keyof Api["endpoints"],
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema
-> = ApiGetEndpointBody<
-  Api,
-  Endpoint
-> extends infer Param extends ApiDataParameter
-  ? InferInputTypeFromSchema<
-      NonNullable<
-        ApiGetEndpointBodySchemaType<
-          Api,
-          Endpoint,
-          DefaultSchemaType
-        >["_provider"]
-      >,
-      Param["schema"]
-    >
-  : InferInputTypeFromSchema<
-      NonNullable<
-        ApiGetEndpointSchemaType<Api, Endpoint, DefaultSchemaType>["_provider"]
-      >,
-      ApiGetEndpointBody<Api, Endpoint>
-    >;
-
-export type ApiInferEndpointParam<
-  Api extends ApiSpec,
-  Endpoint extends keyof Api["endpoints"],
-  PathParam extends keyof ApiGetEndpointParams<Api, Endpoint>,
-  DefaultSchemaType extends SchemaType = typeof ApiTypeScriptSchema,
-  ComputedParam = ApiGetEndpointParams<Api, Endpoint>[PathParam]
-> = ComputedParam extends ApiParameter
-  ? InferInputTypeFromSchema<
-      NonNullable<
-        ApiGetEndpointParamSchemaType<
-          Api,
-          Endpoint,
-          PathParam,
-          DefaultSchemaType
-        >["_provider"]
-      >,
-      ComputedParam["schema"]
-    >
-  : InferInputTypeFromSchema<
-      NonNullable<
-        ApiGetEndpointParamSchemaType<
-          Api,
-          Endpoint,
-          PathParam,
-          DefaultSchemaType
-        >["_provider"]
-      >,
-      ComputedParam
-    >;
