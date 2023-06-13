dac6d91065b31b4e8447cc8af520eeb9e7964664
ecyrbe <ecyrbe@gmail.com>
2023-06-13 23:00:01 +0200

    docs(schema-type): add schema type documentation

--- src/schema-type.types.ts
+++ src/schema-type.types.ts
@@ -1,22 +1,60 @@
+/**
+ * Type Provider for HKT (Higher Kinded Types) in TypeScript
+ * @param Schema - The schema type to resolve into native typescript types
+ */
 export interface ApiTypeProvider<Schema = unknown> {
+  /**
+   * The schema type to resolve into native typescript types
+   */
   schema: Schema;
+  /**
+   * placeholder for resolved input type from the schema
+   */
   input: unknown;
+  /**
+   * placeholder for resolved output type from the schema
+   */
   output: unknown;
 }
+
+/**
+ * Infer the input type from a schema using the type provider
+ * @param TypeProvider - The type provider to use to resolve the schema
+ * @param Schema - The schema to resolve
+ * @returns - the typescript input type for the provided schema
+ */
 export type InferInputTypeFromSchema<
   TypeProvider extends ApiTypeProvider,
   Schema
 > = (TypeProvider & { schema: Schema })["input"];
 
+/**
+ * Infer the output type from a schema using the type provider
+ * @param TypeProvider - The type provider to use to resolve the schema
+ * @param Schema - The schema to resolve
+ * @returns - the typescript output type for the provided schema
+ */
 export type InferOutputTypeFromSchema<
   TypeProvider extends ApiTypeProvider,
   Schema
 > = (TypeProvider & { schema: Schema })["output"];
 
+/**
+ * Interface for runtime schema validation
+ * This is used as a common return type for all schema validation adapters
+ */
 export type SchemaValidationResult =
   | { success: true; data: any }
   | { success: false; error: any };
 
+/**
+ * Schema Type interface
+ * It's used to define a schema validation adapter for both compile time and runtime
+ *
+ * _provider is undefined at runtime, it's used to old the type provider at compile time
+ *
+ * @param TypeProvider - The type provider for the schema type
+ */
 export interface SchemaType<
   TypeProvider extends ApiTypeProvider = ApiTypeProvider
 > {
