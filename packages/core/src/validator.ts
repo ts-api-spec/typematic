543424e36a46e861aeebad83a097cb3d5862b8d5
ecyrbe <ecyrbe@gmail.com>
2023-08-03 19:24:02 +0200

    feat(core): add runtime validators

---
+++ packages/core/src/validator.ts
@@ -0,0 +1,429 @@
+import {
+  apiGetEndpoint,
+  apiGetEndpointByPath,
+  apiGetEndpointBody,
+  apiGetEndpointBodyByPath,
+  apiGetEndpointBodySchema,
+  apiGetEndpointBodySchemaByPath,
+  apiGetEndpointEntrySchema,
+  apiGetEndpointEntry,
+  apiGetEndpointEntryByPath,
+  apiGetEndpointEntrySchemaByPath,
+} from "./parser";
+import type {
+  ApiGetPathsByMethod,
+  ApiGetEndpoint,
+  ApiGetEndpointBodySchema,
+  ApiGetEndpointBodySchemaByPath,
+  ApiGetEndpointByPath,
+  ApiGetEndpointEntrySchema,
+  ApiGetEndpointEntrySchemaByPath,
+} from "./parser.types";
+import { ApiTypeScriptSchema } from "./schema-type-ts";
+import type { SchemaType, SchemaValidationResult } from "./schema-type.types";
+import type { ApiEntry, ApiMethod, ApiSpec } from "./types";
+import type {
+  ApiGetSchemaType,
+  ApiGetEndpointSchemaType,
+  ApiGetEndpointSchemaTypeByPath,
+  ApiGetEndpointBodySchemaType,
+  ApiGetEndpointBodySchemaTypeByPath,
+  ApiGetEndpointEntrySchemaType,
+  ApiGetEndpointEntrySchemaTypeByPath,
+} from "./validator.types";
+
+let defaultSchemaType: SchemaType = ApiTypeScriptSchema;
+
+export function setDefaultSchemaType(schemaType: SchemaType) {
+  defaultSchemaType = schemaType;
+}
+
+export function getDefaultSchemaType() {
+  return defaultSchemaType;
+}
+
+export function apiGetSchemaType<Api extends ApiSpec>(api: Api): ApiGetSchemaType<Api> {
+  return (api.metadata?.schemaType ?? defaultSchemaType) as any;
+}
+
+export function apiGetEndpointSchemaType<Api extends ApiSpec, Endpoint extends keyof Api["endpoints"]>(
+  api: Api,
+  endpoint: Endpoint
+): ApiGetEndpointSchemaType<Api, Endpoint> {
+  return (apiGetEndpoint(api, endpoint).metadata?.schemaType ?? apiGetSchemaType(api)) as any;
+}
+
+export function apiGetEndpointSchemaTypeByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>
+>(api: Api, method: Method, path: Path): ApiGetEndpointSchemaTypeByPath<Api, Method, Path> {
+  return (apiGetEndpointByPath(api, method, path).metadata?.schemaType ?? apiGetSchemaType(api)) as any;
+}
+
+export function apiGetEndpointBodySchemaType<Api extends ApiSpec, Endpoint extends keyof Api["endpoints"]>(
+  api: Api,
+  endpoint: Endpoint
+): ApiGetEndpointBodySchemaType<Api, Endpoint> {
+  const body = apiGetEndpointBody(api, endpoint) as any;
+  return body.metadata?.schemaType ?? apiGetEndpointSchemaType(api, endpoint);
+}
+
+export function apiGetEndpointBodySchemaTypeByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>
+>(api: Api, method: Method, path: Path): ApiGetEndpointBodySchemaTypeByPath<Api, Method, Path> {
+  const body = apiGetEndpointBodyByPath(api, method, path) as any;
+  return body?.metadata?.schemaType ?? apiGetEndpointSchemaTypeByPath(api, method, path);
+}
+
+export function apiGetEndpointEntrySchemaType<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpoint<Api, Endpoint>[Entry]
+>(
+  api: Api,
+  endpoint: Endpoint,
+  entry: Entry,
+  param: EntryParam
+): ApiGetEndpointEntrySchemaType<Api, Endpoint, Entry, EntryParam> {
+  const $entry = apiGetEndpointEntry(api, endpoint, entry, param) as any;
+  return $entry?.metadata?.schemaType ?? apiGetEndpointSchemaType(api, endpoint);
+}
+
+export function apiGetEndpointEntrySchemaTypeByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpointByPath<Api, Method, Path>[Entry]
+>(
+  api: Api,
+  method: Method,
+  path: Path,
+  entry: Entry,
+  param: EntryParam
+): ApiGetEndpointEntrySchemaTypeByPath<Api, Method, Path, Entry, EntryParam> {
+  const $entry = apiGetEndpointEntryByPath(api, method, path, entry, param) as any;
+  return $entry?.metadata?.schemaType ?? apiGetEndpointSchemaTypeByPath(api, method, path);
+}
+
+export function apiValidateEndpointBody<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  $Schema = ApiGetEndpointBodySchema<Api, Endpoint>,
+  $SchemaType extends SchemaType = ApiGetEndpointBodySchemaType<Api, Endpoint>
+>(api: Api, endpoint: Endpoint, body: unknown): SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>> {
+  const schemaType = apiGetEndpointBodySchemaType(api, endpoint);
+  const schema = apiGetEndpointBodySchema(api, endpoint);
+  // @ts-ignore
+  return schemaType.validate(schema, body);
+}
+
+export function apiValidateAsyncEndpointBody<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  $Schema = ApiGetEndpointBodySchema<Api, Endpoint>,
+  $SchemaType extends SchemaType = ApiGetEndpointBodySchemaType<Api, Endpoint>
+>(
+  api: Api,
+  endpoint: Endpoint,
+  body: unknown
+): Promise<SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>>> {
+  const schemaType = apiGetEndpointBodySchemaType(api, endpoint);
+  const schema = apiGetEndpointBodySchema(api, endpoint);
+  // @ts-ignore
+  return schemaType.validateAsync(schema, body);
+}
+
+export function apiValidateEndpointBodyByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  $Schema = ApiGetEndpointBodySchemaByPath<Api, Method, Path>,
+  $SchemaType extends SchemaType = ApiGetEndpointBodySchemaTypeByPath<Api, Method, Path>
+>(
+  api: Api,
+  method: Method,
+  path: Path,
+  body: unknown
+): SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>> {
+  const schemaType = apiGetEndpointBodySchemaTypeByPath(api, method, path);
+  const schema = apiGetEndpointBodySchemaByPath(api, method, path);
+  // @ts-ignore
+  return schemaType.validate(schema, body);
+}
+
+export function apiValidateAsyncEndpointBodyByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  $Schema = ApiGetEndpointBodySchemaByPath<Api, Method, Path>,
+  $SchemaType extends SchemaType = ApiGetEndpointBodySchemaTypeByPath<Api, Method, Path>
+>(
+  api: Api,
+  method: Method,
+  path: Path,
+  body: unknown
+): Promise<SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>>> {
+  const schemaType = apiGetEndpointBodySchemaTypeByPath(api, method, path);
+  const schema = apiGetEndpointBodySchemaByPath(api, method, path);
+  // @ts-ignore
+  return schemaType.validateAsync(schema, body);
+}
+
+export function apiValidateEndpointEntry<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpoint<Api, Endpoint>[Entry],
+  $Schema = ApiGetEndpointEntrySchema<Api, Endpoint, Entry, EntryParam>,
+  $SchemaType extends SchemaType = ApiGetEndpointEntrySchemaType<Api, Endpoint, Entry, EntryParam>
+>(
+  api: Api,
+  endpoint: Endpoint,
+  entry: Entry,
+  param: EntryParam,
+  value: unknown
+): SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>> {
+  const schemaType = apiGetEndpointEntrySchemaType(api, endpoint, entry, param);
+  const schema = apiGetEndpointEntrySchema(api, endpoint, entry, param);
+  // @ts-ignore
+  return schemaType.validate(schema, value);
+}
+
+export function apiValidateAsyncEndpointEntry<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpoint<Api, Endpoint>[Entry],
+  $Schema = ApiGetEndpointEntrySchema<Api, Endpoint, Entry, EntryParam>,
+  $SchemaType extends SchemaType = ApiGetEndpointEntrySchemaType<Api, Endpoint, Entry, EntryParam>
+>(
+  api: Api,
+  endpoint: Endpoint,
+  entry: Entry,
+  param: EntryParam,
+  value: unknown
+): Promise<SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>>> {
+  const schemaType = apiGetEndpointEntrySchemaType(api, endpoint, entry, param);
+  const schema = apiGetEndpointEntrySchema(api, endpoint, entry, param);
+  // @ts-ignore
+  return schemaType.validateAsync(schema, value);
+}
+
+export function apiValidateEndpointEntryByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpointByPath<Api, Method, Path>[Entry],
+  $Schema = ApiGetEndpointEntrySchemaByPath<Api, Method, Path, Entry, EntryParam>,
+  $SchemaType extends SchemaType = ApiGetEndpointEntrySchemaTypeByPath<Api, Method, Path, Entry, EntryParam>
+>(
+  api: Api,
+  method: Method,
+  path: Path,
+  entry: Entry,
+  param: EntryParam,
+  value: unknown
+): SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>> {
+  const schemaType = apiGetEndpointEntrySchemaTypeByPath(api, method, path, entry, param);
+  const schema = apiGetEndpointEntrySchemaByPath(api, method, path, entry, param);
+  // @ts-ignore
+  return schemaType.validate(schema, value);
+}
+
+export function apiValidateAsyncEndpointEntryByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  Entry extends ApiEntry,
+  EntryParam extends keyof ApiGetEndpointByPath<Api, Method, Path>[Entry],
+  $Schema = ApiGetEndpointEntrySchemaByPath<Api, Method, Path, Entry, EntryParam>,
+  $SchemaType extends SchemaType = ApiGetEndpointEntrySchemaTypeByPath<Api, Method, Path, Entry, EntryParam>
+>(
+  api: Api,
+  method: Method,
+  path: Path,
+  entry: Entry,
+  param: EntryParam,
+  value: unknown
+): Promise<SchemaValidationResult<$Schema, NonNullable<$SchemaType["_provider"]>>> {
+  const schemaType = apiGetEndpointEntrySchemaTypeByPath(api, method, path, entry, param);
+  const schema = apiGetEndpointEntrySchemaByPath(api, method, path, entry, param);
+  // @ts-ignore
+  return schemaType.validateAsync(schema, value);
+}
+
+export function apiValidateEndpointParam<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  PathParam extends keyof ApiGetEndpoint<Api, Endpoint>["params"]
+>(api: Api, endpoint: Endpoint, pathParam: PathParam, value: unknown) {
+  return apiValidateEndpointEntry(api, endpoint, "params", pathParam, value);
+}
+
+export function apiValidateAsyncEndpointParam<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  PathParam extends keyof ApiGetEndpoint<Api, Endpoint>["params"]
+>(api: Api, endpoint: Endpoint, pathParam: PathParam, value: unknown) {
+  return apiValidateAsyncEndpointEntry(api, endpoint, "params", pathParam, value);
+}
+
+export function apiValidateEndpointParamByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  PathParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["params"]
+>(api: Api, method: Method, path: Path, pathParam: PathParam, value: unknown) {
+  return apiValidateEndpointEntryByPath(api, method, path, "params", pathParam, value);
+}
+
+export function apiValidateAsyncEndpointParamByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  PathParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["params"]
+>(api: Api, method: Method, path: Path, pathParam: PathParam, value: unknown) {
+  return apiValidateAsyncEndpointEntryByPath(api, method, path, "params", pathParam, value);
+}
+
+export function apiValidateEndpointQuery<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  QueryParam extends keyof ApiGetEndpoint<Api, Endpoint>["query"]
+>(api: Api, endpoint: Endpoint, queryParam: QueryParam, value: unknown) {
+  return apiValidateEndpointEntry(api, endpoint, "query", queryParam, value);
+}
+
+export function apiValidateAsyncEndpointQuery<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  QueryParam extends keyof ApiGetEndpoint<Api, Endpoint>["query"]
+>(api: Api, endpoint: Endpoint, queryParam: QueryParam, value: unknown) {
+  return apiValidateAsyncEndpointEntry(api, endpoint, "query", queryParam, value);
+}
+
+export function apiValidateEndpointQueryByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  QueryParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["query"]
+>(api: Api, method: Method, path: Path, queryParam: QueryParam, value: unknown) {
+  return apiValidateEndpointEntryByPath(api, method, path, "query", queryParam, value);
+}
+
+export function apiValidateAsyncEndpointQueryByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  QueryParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["query"]
+>(api: Api, method: Method, path: Path, queryParam: QueryParam, value: unknown) {
+  return apiValidateAsyncEndpointEntryByPath(api, method, path, "query", queryParam, value);
+}
+
+export function apiValidateEndpointHeader<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  HeaderParam extends keyof ApiGetEndpoint<Api, Endpoint>["headers"]
+>(api: Api, endpoint: Endpoint, headerParam: HeaderParam, value: unknown) {
+  return apiValidateEndpointEntry(api, endpoint, "headers", headerParam, value);
+}
+
+export function apiValidateAsyncEndpointHeader<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  HeaderParam extends keyof ApiGetEndpoint<Api, Endpoint>["headers"]
+>(api: Api, endpoint: Endpoint, headerParam: HeaderParam, value: unknown) {
+  return apiValidateAsyncEndpointEntry(api, endpoint, "headers", headerParam, value);
+}
+
+export function apiValidateEndpointHeaderByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  HeaderParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["headers"]
+>(api: Api, method: Method, path: Path, headerParam: HeaderParam, value: unknown) {
+  return apiValidateEndpointEntryByPath(api, method, path, "headers", headerParam, value);
+}
+
+export function apiValidateAsyncEndpointHeaderByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  HeaderParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["headers"]
+>(api: Api, method: Method, path: Path, headerParam: HeaderParam, value: unknown) {
+  return apiValidateAsyncEndpointEntryByPath(api, method, path, "headers", headerParam, value);
+}
+
+export function apiValidateEndpointCookie<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  CookieParam extends keyof ApiGetEndpoint<Api, Endpoint>["cookies"]
+>(api: Api, endpoint: Endpoint, cookieParam: CookieParam, value: unknown) {
+  return apiValidateEndpointEntry(api, endpoint, "cookies", cookieParam, value);
+}
+
+export function apiValidateAsyncEndpointCookie<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  CookieParam extends keyof ApiGetEndpoint<Api, Endpoint>["cookies"]
+>(api: Api, endpoint: Endpoint, cookieParam: CookieParam, value: unknown) {
+  return apiValidateAsyncEndpointEntry(api, endpoint, "cookies", cookieParam, value);
+}
+
+export function apiValidateEndpointCookieByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  CookieParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["cookies"]
+>(api: Api, method: Method, path: Path, cookieParam: CookieParam, value: unknown) {
+  return apiValidateEndpointEntryByPath(api, method, path, "cookies", cookieParam, value);
+}
+
+export function apiValidateAsyncEndpointCookieByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  CookieParam extends keyof ApiGetEndpointByPath<Api, Method, Path>["cookies"]
+>(api: Api, method: Method, path: Path, cookieParam: CookieParam, value: unknown) {
+  return apiValidateAsyncEndpointEntryByPath(api, method, path, "cookies", cookieParam, value);
+}
+
+export function apiValidateEndpointResponse<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  StatusCode extends keyof ApiGetEndpoint<Api, Endpoint>["responses"]
+>(api: Api, endpoint: Endpoint, statusCode: StatusCode, value: unknown) {
+  return apiValidateEndpointEntry(api, endpoint, "responses", statusCode, value);
+}
+
+export function apiValidateAsyncEndpointResponse<
+  Api extends ApiSpec,
+  Endpoint extends keyof Api["endpoints"],
+  StatusCode extends keyof ApiGetEndpoint<Api, Endpoint>["responses"]
+>(api: Api, endpoint: Endpoint, statusCode: StatusCode, value: unknown) {
+  return apiValidateAsyncEndpointEntry(api, endpoint, "responses", statusCode, value);
+}
+
+export function apiValidateEndpointResponseByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  StatusCode extends keyof ApiGetEndpointByPath<Api, Method, Path>["responses"]
+>(api: Api, method: Method, path: Path, statusCode: StatusCode, value: unknown) {
+  return apiValidateEndpointEntryByPath(api, method, path, "responses", statusCode, value);
+}
+
+export function apiValidateAsyncEndpointResponseByPath<
+  Api extends ApiSpec,
+  Method extends ApiMethod,
+  Path extends ApiGetPathsByMethod<Api, Method>,
+  StatusCode extends keyof ApiGetEndpointByPath<Api, Method, Path>["responses"]
+>(api: Api, method: Method, path: Path, statusCode: StatusCode, value: unknown) {
+  return apiValidateAsyncEndpointEntryByPath(api, method, path, "responses", statusCode, value);
+}
